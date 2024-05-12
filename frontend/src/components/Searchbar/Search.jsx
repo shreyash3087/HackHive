6e96c5d76f0d38a3ce9115939cdacc33d2892b5d
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-05-12 11:32:29 +0530

    removed unused css files

--- frontend/src/components/Searchbar/Search.jsx
+++ frontend/src/components/Searchbar/Search.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-// import "./Search.css"
+import React from "react";
 
 export default function Search() {
   return (
     <div className="flex justify-center items-center">
-        <input type="text" id='search-bar' className="border border-black h-12 w-80 px-4 rounded-lg" placeholder='Search for Hackathons' />
-        <button className='bg-blue-500 text-white rounded-lg hover:shadow-md h-12 w-24 ml-12'>Search</button>
+      <input
+        type="text"
+        id="search-bar"
+        className="border border-black h-12 w-80 px-4 rounded-lg"
+        placeholder="Search for Hackathons"
+      />
+      <button className="bg-blue-500 text-white rounded-lg hover:shadow-md h-12 w-24 ml-12">
+        Search
+      </button>
     </div>
-  )
+  );
 }
