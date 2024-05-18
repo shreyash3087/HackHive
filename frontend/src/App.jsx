c917b47f305ab0d1488ad3a6a402c382bf7e94ce
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-05-18 17:03:05 +0530

    [Feature] Added Custom 404 page (#127)
    * background images added
    
    * background images added
    
    * Custom 404 page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,59 +1,61 @@
-import { useState, useEffect } from "react";
-import Login from "./Pages/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./Pages/Landing";
-import Hackathons from "./Pages/Hackathons";
-import BlogsPage from "./Pages/Blog";
+import { useState, useEffect } from 'react';
+import Login from './Pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './Pages/Landing';
+import Hackathons from './Pages/Hackathons';
+import BlogsPage from './Pages/Blog';
+import Custom404 from './Pages/Custom404';
 
 function App() {
-	const [darkMode, setDarkMode] = useState(() => {
-		const savedMode = localStorage.getItem("darkMode");
-		return savedMode === "true" ? true : false;
-	});
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    return savedMode === 'true' ? true : false;
+  });
 
-	const toggleDarkMode = () => {
-		setDarkMode((prevMode) => {
-			const newMode = !prevMode;
-			localStorage.setItem("darkMode", JSON.stringify(newMode));
-			return newMode;
-		});
-	};
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem('darkMode', JSON.stringify(newMode));
+      return newMode;
+    });
+  };
 
-	useEffect(() => {
-		if (darkMode) {
-			document.documentElement.classList.add("dark");
-		} else {
-			document.documentElement.classList.remove("dark");
-		}
-	}, [darkMode]);
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
 
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path='/'
-						element={
-							<Landing darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-						}
-					/>
-					<Route path='/login' element={<Login />} />
-					<Route
-						path='/hackathons'
-						element={
-							<Hackathons darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-						}
-					/>
-					<Route
-						path='/blogs'
-						element={
-							<BlogsPage darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-						}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Landing darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/hackathons"
+            element={
+              <Hackathons darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            }
+          />
+          <Route
+            path="/blogs"
+            element={
+              <BlogsPage darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            }
+          />
+          <Route path="*" element={<Custom404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
